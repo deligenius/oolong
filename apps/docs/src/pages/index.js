0013918af170c1bd9afd3ced525ce1e9cc069a70
Jun Guo <gjuoun@gmail.com>
2023-06-19 03:48:50 -0400

    embed code

--- apps/docs/src/pages/index.js
+++ apps/docs/src/pages/index.js
@@ -8,26 +8,33 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
-        </div>
+        </div> */}
+
+        <iframe src="https://codesandbox.io/embed/modern-sun-wzx37h?fontsize=14&hidenavigation=1&theme=dark"
+        style={{ width: "100%", height: "500px", border: 0, borderRadius: "4px", overflow: "hidden" }}
+          title="modern-sun-wzx37h"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
